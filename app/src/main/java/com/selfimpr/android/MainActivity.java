70347726d89628ff93859fec70c202c0035179fc
wangjiacheng <wangjiacheng@58ganji.com>
2018-05-21 20:38:23 +0800

    可重入锁

--- app/src/main/java/com/selfimpr/android/MainActivity.java
+++ app/src/main/java/com/selfimpr/android/MainActivity.java
@@ -2,8 +2,8 @@ package com.selfimpr.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +14,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public final void helloWorld(final View view) {
-
-
+    //可重入锁测试
+    public synchronized void test1() {
+        test2();
+        Log.e("test", "test1");
     }
 
+    private synchronized void test2() {
+        Log.e("test", "test2");
+    }
+
+    public void clickImageView(View view) {
+        test1();
+    }
 }
