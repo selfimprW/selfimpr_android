1ef194b611d01db84f74babc052ad01f811152fb
wangjiacheng <wangjiacheng@58ganji.com>
2018-06-13 19:24:53 +0800

    增加通用lib

--- volleydemo/src/main/java/com/selfimpr/volleydemo/MainActivity.java
+++ volleydemo/src/main/java/com/selfimpr/volleydemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.selfimpr.volleydemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,23 +14,32 @@ import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
+    RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue = Volley.newRequestQueue(this);
+    }
+
+    public void request(View view) {
         StringRequest stringRequest = new StringRequest("https://vipshop.github.io/vjtools/#/standard/chapter09", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
                 Log.d("StringRequest", response);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
                 Log.e("StringRequest", error.getMessage(), error);
             }
         });
+        //设置缓存
+        stringRequest.setShouldCache(true);
         requestQueue.add(stringRequest);
     }
 }
