698e0afd506507f293b1763e4ad37e52d4351c60
wangjiacheng <wangjiacheng@zhuanzhuan.com>
2018-06-17 00:04:06 +0800

    存储module

--- StorageDemo/src/main/java/com/selfimpr/storagedemo/MainActivity.java
+++ StorageDemo/src/main/java/com/selfimpr/storagedemo/MainActivity.java
@@ -1,84 +1,58 @@
 package com.selfimpr.storagedemo;
 
+import android.app.usage.ExternalStorageStats;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+/**
+ * 一、是一个应用程序的私有目录，只有当前应用程序有权限访问读写，其他应用无权限访问。
+ * 一些安全性要求比较高的数据存放在该目录，一般用来存放size比较小的数据。
+ * <p>
+ * 二、是一个外部存储目录，只用应用声明了<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>的一个权限，
+ * 就可以访问读写sdcard目录；所以一般用来存放一些安全性不高的数据，文件size比较大的数据。
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+      File  file = new File(getExternalFilesDir(""),"sss");
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            File sdCardDir = Environment.getExternalStorageDirectory();//获取sd卡目录
+            File sdFile  = new File(sdCardDir, "wangjiacheng");
+        }
+
+        StorageUtil.writeString(file, "wangjiacheng");
+        String value = StorageUtil.readString(file);
+        Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+
+
+//        openFileInput()-内部实现->FileInputStream
+
         Person person = new Person();
         person.setName("wangjiacheng");
+//        File fileObject = new File( "object1.txt");
+        StorageUtil.readObject(this, "obj1ect.txt");
 
-        File file = new File(getCacheDir(), "aaaa.txt");
+        StorageUtil.writeObject(this, "ob1ject.txt", person);
 
-        write(file);
-
-        String value = read(file);
-        Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
     }
 
-
-    private void write(File file) {
-        FileOutputStream outStream = null;
-        try {
-            outStream = new FileOutputStream(file);
-            outStream.write("wangjiacheng".getBytes());
-            outStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (outStream != null) {
-                try {
-                    outStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public String read(File file) {
-        StringBuilder sb = new StringBuilder();
-        //打开文件输入流
-        FileInputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(file);
-            byte[] temp = new byte[1024];
-            int len = 0;
-            //读取文件内容:
-            while ((len = inputStream.read(temp)) > 0) {
-                sb.append(new String(temp, 0, len));
-            }
-            //关闭输入流
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return sb.toString();
-    }
 
 }
---
+++ StorageDemo/src/main/java/com/selfimpr/storagedemo/StorageUtil.java
@@ -0,0 +1,152 @@
+package com.selfimpr.storagedemo;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * description：  FileWriter/FileReader；
+ * JsonReader；InputStreamReader；
+ * BufferedInputStream|ByteArrayOutputStream|BufferedOutputStream
+ * BufferedOutputStream|ByteArrayInputStream|BufferedInputStream<br/>
+ * ===============================<br/>
+ * creator：Jiacheng<br/>
+ * create time：2018/6/16 23:01<br/>
+ * ===============================<br/>
+ * reasons for modification：  <br/>
+ * Modifier：  <br/>
+ * Modify time：  <br/>
+ */
+public class StorageUtil {
+
+    /**
+     * @param context  context
+     * @param fileName 文件名，不能是路径
+     * @param obj      对象
+     * @return true是存储成功
+     */
+    public static boolean writeObject(Context context, String fileName, Object obj) {
+        boolean status = true;
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            // MODE_APPEND：即向文件尾写入数据;
+            // MODE_PRIVATE：即仅打开文件可写入数据;
+            // MODE_WORLD_READABLE：所有程序均可读该文件数据;
+            // MODE_WORLD_WRITABLE：即所有程序均可写入数据。
+            fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+            status = false;
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (oos != null) {
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return status;
+    }
+
+    /**
+     * @param context  context
+     * @param fileName 文件名，不能是路径
+     * @return 获取存储对象
+     */
+    public static Object readObject(Context context, String fileName) {
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            File streamFile = context.getFileStreamPath(fileName);
+            if (streamFile != null && streamFile.exists() && streamFile.isFile() && streamFile.length() > 0) {
+                fis = context.openFileInput(fileName);
+                ois = new ObjectInputStream(fis);
+                return ois.readObject();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean writeString(File file, String content) {
+        if (TextUtils.isEmpty(content)) {
+            return true;
+        }
+        boolean status = true;
+        FileOutputStream outStream = null;
+        try {
+            outStream = new FileOutputStream(file);
+            outStream.write(content.getBytes());
+            outStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            status = false;
+        } finally {
+            if (outStream != null) {
+                try {
+                    outStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return status;
+    }
+
+    public static String readString(File file) {
+        StringBuilder sb = new StringBuilder();
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            byte[] temp = new byte[1024];
+            int len = 0;
+            while ((len = inputStream.read(temp)) > 0) {
+                sb.append(new String(temp, 0, len));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
