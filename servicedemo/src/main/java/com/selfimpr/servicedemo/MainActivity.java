fa8d0135632c451b367cd57dc95e11be5aa52c5a
wangjiacheng <wangjiacheng@58ganji.com>
2018-06-19 15:49:40 +0800

    service demo

--- servicedemo/src/main/java/com/selfimpr/servicedemo/MainActivity.java
+++ servicedemo/src/main/java/com/selfimpr/servicedemo/MainActivity.java
@@ -3,13 +3,16 @@ package com.selfimpr.servicedemo;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+//https://blog.csdn.net/javazejian/article/details/52709857
 public class MainActivity extends AppCompatActivity {
+
+    private SelfimprService selfimprService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +44,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     ServiceConnection serviceConnection = new ServiceConnection() {
+
+        /**
+         * 在操作者在连接一个服务成功时被调用
+         * @param name ComponentName是一个封装了组件(Activity, Service, BroadcastReceiver,
+         *            or ContentProvider)信息的类，如包名，组件描述等信息，较少使用该参数。
+         * @param service service便是服务端返回的IBinder实现类对象，通过该对象我们便可以调用获取SelfimprService实例对象，进而调用服务端的公共方法。
+         */
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             Log.e("SelfimprService", "onServiceConnected--->ComponentName:" + name);
+            SelfimprService.LocalBinder binder = (SelfimprService.LocalBinder) service;
+            selfimprService = binder.getService();
         }
 
+        /**
+         * Android 系统会在与服务的连接意外中断时（例如当服务崩溃或被终止时）调用该方法。注意:当客户端取消绑定时，系统“绝对不会”调用该方法。
+         */
         @Override
         public void onServiceDisconnected(ComponentName name) {
             Log.e("SelfimprService", "onServiceDisconnected--->ComponentName:" + name);
+            selfimprService = null;
         }
     };
 }
--- servicedemo/src/main/java/com/selfimpr/servicedemo/SelfimprService.java
+++ servicedemo/src/main/java/com/selfimpr/servicedemo/SelfimprService.java
@@ -2,12 +2,14 @@ package com.selfimpr.servicedemo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 /**
- * description：   <br/>
+ * description：  https://blog.csdn.net/javazejian/article/details/52709857 <br/>
  * ===============================<br/>
  * creator：Jiacheng<br/>
  * create time：2018/6/19 下午3:01<br/>
@@ -27,7 +29,19 @@ public class SelfimprService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         Log.e("SelfimprService", "onBind");
-        return null;
+        return binder;
+    }
+
+    @Override
+    public void unbindService(ServiceConnection conn) {
+        super.unbindService(conn);
+        Log.e("SelfimprService", "unbindService");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.e("SelfimprService", "onUnbind");
+        return super.onUnbind(intent);
     }
 
     @Override
@@ -46,5 +60,18 @@ public class SelfimprService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.e("SelfimprService", "onDestroy");
+    }
+
+    private LocalBinder binder = new LocalBinder();
+
+    /**
+     * 创建Binder对象，返回给客户端即Activity使用，提供数据交换的接口
+     */
+    public class LocalBinder extends Binder {
+        // 声明一个方法，getService。（提供给客户端调用）
+        SelfimprService getService() {
+            // 返回当前对象LocalService,这样我们就可在客户端端调用Service的公共方法了
+            return SelfimprService.this;
+        }
     }
 }
