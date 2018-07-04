dc170e550af71eb078cac844a4739d94c126f7c8
wangjiacheng <wangjiacheng@58ganji.com>
2018-07-04 10:46:59 +0800

    inputType

--- keyborad/src/main/java/com/selfimpr/keyborad/MainActivity.java
+++ keyborad/src/main/java/com/selfimpr/keyborad/MainActivity.java
@@ -2,14 +2,20 @@ package com.selfimpr.keyborad;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        edit = findViewById(R.id.edit);
+        edit.setInputType(EditorInfo.TYPE_NUMBER_FLAG_DECIMAL);
     }
 }
