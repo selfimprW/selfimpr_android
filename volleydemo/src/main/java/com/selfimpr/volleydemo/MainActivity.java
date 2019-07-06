3a352c92c1554444da9357ce4028e4c759f782c6
wangjiacheng <selfimpr@yeah.net>
2019-07-06 14:19:50 +0800

    volley引入方式

--- volleydemo/src/main/java/com/selfimpr/volleydemo/MainActivity.java
+++ volleydemo/src/main/java/com/selfimpr/volleydemo/MainActivity.java
@@ -6,6 +6,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
     RequestQueue requestQueue;
