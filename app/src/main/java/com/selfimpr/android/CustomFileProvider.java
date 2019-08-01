48ed93608f32be813d18a278eabce4b690963a73
wangjiacheng <selfimpr@yeah.net>
2019-08-01 22:16:53 +0800

    FileProvider

---
+++ app/src/main/java/com/selfimpr/android/CustomFileProvider.java
@@ -0,0 +1,16 @@
+package com.selfimpr.android;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * description：https://developer.android.google.cn/reference/android/support/v4/content/FileProvider.html#more-information   <br/>
+ * ===============================<br/>
+ * creator：Jiacheng<br/>
+ * create time：2019-08-01 22:13<br/>
+ * ===============================<br/>
+ * reasons for modification：  <br/>
+ * Modifier：  <br/>
+ * Modify time：  <br/>
+ */
+public class CustomFileProvider extends FileProvider {
+}
